9c28166a53bdb8ae3ba0e38b3bb9474d15d2f57a
WILL <13824693926@163.com>
2022-08-14 22:27:46 +0800

    version 0.5

--- app/src/main/java/com/example/projectmonitoringapp/UserLoginActivity.java
+++ app/src/main/java/com/example/projectmonitoringapp/UserLoginActivity.java
@@ -2,7 +2,13 @@ package com.example.projectmonitoringapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class UserLoginActivity extends AppCompatActivity {
 
@@ -10,5 +16,36 @@ public class UserLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        TextView caretakerLoginTextView = (TextView) findViewById(R.id.caretaker_login);
+        caretakerLoginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UserLoginActivity.this,CaretakerLogin.class));
+            }
+        });
+
+        TextView registerTextView = (TextView) findViewById(R.id.register);
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UserLoginActivity.this,RegisterActivity.class));
+            }
+        });
+
+        Button loginButton = (Button) findViewById(R.id.bt_login);
+        EditText usernameEditText = (EditText) findViewById(R.id.et_yhm);
+        EditText passwordEditText = (EditText) findViewById(R.id.et_mm);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ("".equals(usernameEditText.getText().toString())) {
+                    Toast.makeText(UserLoginActivity.this,"用户名不能为空！",Toast.LENGTH_SHORT).show();
+                } else {
+
+                }
+            }
+        });
+
     }
 }
