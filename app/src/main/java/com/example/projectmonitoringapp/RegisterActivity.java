3047c778994c8a30d3c5726225cab0d9dc2350e8
WILL <13824693926@163.com>
2022-08-17 14:16:26 +0800

    version 0.9

--- app/src/main/java/com/example/projectmonitoringapp/RegisterActivity.java
+++ app/src/main/java/com/example/projectmonitoringapp/RegisterActivity.java
@@ -1,14 +1,28 @@
 package com.example.projectmonitoringapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.projectmonitoringapp.model.LoginReceive;
+import com.example.projectmonitoringapp.model.Register;
+import com.example.projectmonitoringapp.util.HttpTool;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class RegisterActivity extends AppCompatActivity {
     @Override
@@ -39,6 +53,7 @@ public class RegisterActivity extends AppCompatActivity {
         EditText passwordEditText = (EditText) findViewById(R.id.et_mm);
         EditText passwordSureEditText = (EditText) findViewById(R.id.et_qrmm);
         registerButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
                 if ("".equals(usernameEditText.getText().toString())) {
@@ -48,10 +63,41 @@ public class RegisterActivity extends AppCompatActivity {
                 } else if ("".equals(mailEditText.getText().toString())) {
                     Toast.makeText(RegisterActivity.this,"邮箱不能为空！",Toast.LENGTH_SHORT).show();
                 } else {
-
+                    sendRegister(Crypt2.getRandomString(16),new Gson().toJson(new Register(usernameEditText.getText().toString(),passwordEditText.getText().toString(),phoneEditText.getText().toString())));
                 }
             }
         });
 
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void sendRegister(String key, String str) {
+        HttpTool.postRegister(key, str, new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(RegisterActivity.this,"连接失败",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                LoginReceive receive = new Gson().fromJson(response.body().string(),LoginReceive.class);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (receive.getCode() == 200) {
+                            Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, receive.getMsg(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/projectmonitoringapp/UserLoginActivity.java
+++ app/src/main/java/com/example/projectmonitoringapp/UserLoginActivity.java
@@ -1,14 +1,29 @@
 package com.example.projectmonitoringapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.projectmonitoringapp.model.LoginReceive;
+import com.example.projectmonitoringapp.model.UserLogin;
+import com.example.projectmonitoringapp.util.HttpTool;
+import com.example.projectmonitoringapp.util.HttpUtil;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class UserLoginActivity extends AppCompatActivity {
 
@@ -38,15 +53,49 @@ public class UserLoginActivity extends AppCompatActivity {
         EditText usernameEditText = (EditText) findViewById(R.id.et_yhm);
         EditText passwordEditText = (EditText) findViewById(R.id.et_mm);
         loginButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
                 if ("".equals(usernameEditText.getText().toString())) {
                     Toast.makeText(UserLoginActivity.this,"用户名不能为空！",Toast.LENGTH_SHORT).show();
                 } else {
-
+                    sendLogin(Crypt2.getRandomString(16),new Gson().toJson(new UserLogin(usernameEditText.getText().toString(),passwordEditText.getText().toString())));
                 }
             }
         });
 
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void sendLogin(String key, String str) {
+        HttpTool.caretakerLogin(key, str, new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(UserLoginActivity.this,"连接失败",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                LoginReceive receive = new Gson().fromJson(response.body().string(),LoginReceive.class);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (receive.getCode() == 200) {
+                            Toast.makeText(UserLoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                            HttpUtil.setToken(response.header("Authorization"));
+                            startActivity(new Intent(UserLoginActivity.this,UserMainActivity.class));
+                        } else {
+                            Toast.makeText(UserLoginActivity.this, receive.getMsg(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
 }
